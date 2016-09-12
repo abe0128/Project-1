5c517ef4a0a547f4b58bc7e5cca3a27f099aecea
abe <adealba@csumb.edu>
2016-09-12 10:32:07 -0700

    first commit

--- project1.py
+++ project1.py
@@ -1,2 +1,6 @@
-#python!
-print("Hello World!")
+from PIL import Image
+filename = "test-house.png"
+img = Image.open(filename)
+new_img = img.resize((256,256))
+new_img.save('test-house-256x256','png')
+new_img.show()
